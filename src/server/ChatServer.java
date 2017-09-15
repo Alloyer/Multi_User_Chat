acaa46f59bfb111064567007ff84e6e497380603
Alloyer <alexeygruntov@yandex.ru>
2017-09-15 20:50:47 +0300

    Настроены пакеты и добавлены базовые классы для работы с БД

--- src/server/ChatServer.java
+++ src/server/ChatServer.java
@@ -1,4 +1,4 @@
-package root.server;
+package server;
 
 import java.io.IOException;
 import java.net.ServerSocket;
--- src/server/ChatServerClient.java
+++ src/server/ChatServerClient.java
@@ -1,4 +1,4 @@
-package root.server;
+package server;
 
 import java.io.*;
 import java.net.Socket;
